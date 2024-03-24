4b554cbecc4c0b2a4c58359975a2883bf7628dc9
Anjana <anjana@dthree.co>
2024-03-24 12:52:27 +0300

    UPDATED CONFIGS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'timetech-client';
+export class AppComponent implements OnInit {
+
+  constructor(private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit() {
+      this.primengConfig.ripple = true;
+  }
 }
+
